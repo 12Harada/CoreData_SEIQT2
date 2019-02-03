6bf49c02cd8adc56d0661a0843f11c783a4dda3e
12harada <12haradaut@gmail.com>
2019-02-03 20:32:18 +0900

    Initial Commit

---
+++ CoreData_SEIQT2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData_SEIQT2
+//
+//  Created by Harada Hiroaki on 2019/02/03.
+//  Copyright © 2019 Harada Hiroaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
